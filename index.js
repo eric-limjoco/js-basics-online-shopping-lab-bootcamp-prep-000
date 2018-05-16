14f20c2c8858b1feda9a02e444aca13d41980856
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-16 11:44:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  if(cart.length === 0) return 'Your shopping cart is empty'
+  if(cart.length === 0) return 'Your shopping cart is empty.'
   var res = 'In your cart, you have '
   
   for(var i=0; i<cart.length; i++){
