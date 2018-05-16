92264d97bd8ee7670199e8bc081b0b94493afd25
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-16 11:55:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,6 +48,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if(cardNumber === '') return "Sorry, we don't have a credit card on file for you."
+  
+  var res = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
 }
 
 
