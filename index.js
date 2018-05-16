e30d912c8cb65916321557ab50a87de26c504eaa
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-16 11:55:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber === '') return "Sorry, we don't have a credit card on file for you."
 }
 
 
